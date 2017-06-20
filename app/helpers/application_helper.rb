82156fac5c394340124110dff5ffed04eb2e7e0d
Derrick Rohl <derrickrohl@gmail.com>
2017-06-20 16:03:07 -0500

    XML to JSON rewrite

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,38 @@
+
 module ApplicationHelper
- 
+
   def get_programs()
-    file = open('http://signs.adventuresci.org/data/rooms.xml').read
-    file.gsub! '<d:', '<'
-    file.gsub! '<m:', '<'
-    xml = Nokogiri::XML(file)
-    return xml
+    user = ENV['altru_user']
+    pass = ENV['altru_pass']
+    link = "https://altrurig01bo3.blackbaudhosting.com/2532Altru_e4a2fe99-d6e1-46e7-a6c0-f6b932aa606d/ODataQuery.ashx?databasename=14AFCB42-1379-457A-AD0C-0CD6166E17F3&AdHocQueryID=b111720e-fa60-4074-8d36-560ffc8a1727"
+    odata = open(link, http_basic_authentication: [user, pass]).read 
+    json = JSON.parse odata 
+    return json
   end
 
-  def populate_from_file(xml,location)
+  def populate_from_file(json,location)
     var = ""
     programsfound = false
     runningshow = "something"
-    xml.css('entry').each do |node|
-      if node.at('ItinerariesItemsProgramEventsLocationsName').text == location
+    
+    json["value"].each do |json|
+      if json['ItinerariesItemsProgramEventsLocationsName'] == location
         var += '<div class="row">'
           var += '<div class="col-sm-6">'
-            if node.at('ItinerariesItemsProgramEventsStarttime').text != runningshow
-              date = DateTime.strptime(node.at('ItinerariesItemsProgramEventsStarttime').text,"%H%M")
-              var += '<strong>' + date.strftime("%-I:%M") + " " + node.at('ItinerariesItemsProgramEventsName').text + '</strong>'
-              runningshow = node.at('ItinerariesItemsProgramEventsStarttime').text
+            if json['ItinerariesItemsProgramEventsStarttime'] != runningshow
+              date = DateTime.strptime(json['ItinerariesItemsProgramEventsStarttime'],"%H%M")
+              var += '<strong>' + date.strftime("%-I:%M") + " " + json['ItinerariesItemsProgramEventsName'] + '</strong>'
+              runningshow = json['ItinerariesItemsProgramEventsStarttime']
             end
           var += '</div>'
           var += '<div class="col-sm-6">'
-            var += node.at('ItinerariesName').text + " (" + node.at('SUMItinerariesAttendeesQuantity').text + ")"
+            var += json['ItinerariesName'] + " (" + json['SUMItinerariesAttendeesQuantity'].to_s + ")"
           var += "</div>"
         var += "</div>"
         programsfound = true
       end
     end
+
     if programsfound == false
       var += '<div class="alert alert-danger" role="alert">
           <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>

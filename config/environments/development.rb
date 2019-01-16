e9f46c7422dc7e6ff555ddf7a92a52a2b070c44a
Derrick Rohl <derrickrohl@gmail.com>
2019-01-15 20:33:27 -0600

    Changing mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,12 +55,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025} 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.office365.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['gmail_user'],
-    password:             ENV['gmail_pass'],
+    domain:               'adventuresci.org',
+    user_name:            ENV['office_user'],
+    password:             ENV['office_pass'],
     authentication:       :plain,
     enable_starttls_auto: true
   }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'http://staff.adventuresci.org/' }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.office365.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['gmail_user'],
-    password:             ENV['gmail_pass'],
+    domain:               'adventuresci.org',
+    user_name:            ENV['office_user'],
+    password:             ENV['office_pass'],
     authentication:       :plain,
     enable_starttls_auto: true
   }
